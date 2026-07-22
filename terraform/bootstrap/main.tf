ad601886f2859cd4d1e903497cea1e819bfa323f
Alagani Jagadeesh <alaganijagadeesh1@gmail.com>
2026-07-22 14:03:55 +0000

    ci: add bootstrap config for Terraform state backend

---
+++ terraform/bootstrap/main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "state" {
+  name     = "rg-terraform-state"
+  location = "East Asia"
+}
+
+resource "azurerm_storage_account" "state" {
+  name                     = "stterraformstatebeastline"
+  resource_group_name      = azurerm_resource_group.state.name
+  location                 = azurerm_resource_group.state.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "state" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.state.name
+  container_access_type = "private"
+}
