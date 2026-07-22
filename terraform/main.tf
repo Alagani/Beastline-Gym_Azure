072c12d72043d331389d69042f2bef81ade2d868
Alagani Jagadeesh <alaganijagadeesh1@gmail.com>
2026-07-22 15:48:34 +0000

    fix: add import block for existing static web app

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,11 @@ import {
   id = "/subscriptions/b2f4370b-28c6-40a2-a873-ae9bf0d6b14f/resourceGroups/rg-staticwebapp-dev"
 }
 
+import {
+  to = azurerm_static_web_app.swa
+  id = "/subscriptions/b2f4370b-28c6-40a2-a873-ae9bf0d6b14f/resourceGroups/rg-staticwebapp-dev/providers/Microsoft.Web/staticSites/my-static-webapp"
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
